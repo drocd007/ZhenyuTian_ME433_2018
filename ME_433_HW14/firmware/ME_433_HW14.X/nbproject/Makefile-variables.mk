c137f14c582add1eaafa90205643c3d39913b46b
Daniel <tzy_sing@hotmail.com>
2018-05-24 18:43:14 -0500

    HW14

---
+++ ME_433_HW14/firmware/ME_433_HW14.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ME_433_HW14.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ME_433_HW14.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=me433hw14.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/me433hw14.x.tar
--- ME_433_HW8/firmware/src/app.c
+++ ME_433_HW8/firmware/src/app.c
@@ -54,6 +54,26 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 // *****************************************************************************
 
 #include "app.h"
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+#include "i2c_master_noint.h"
+#include "ST7735.h"
+#include<stdio.h> //get sprintf
+
+
+    int q = 0;
+    unsigned short data[10];
+
+ 
+    signed short  accelX, accelZ;
+    
+    int percentX, percentZ;
+    
+    int range = 32768;
+    
+    char message[30];
+    char message1[30];
+    char message2[30];
 
 // *****************************************************************************
 // *****************************************************************************
@@ -61,6 +81,8 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 // *****************************************************************************
 // *****************************************************************************
 
+
+#define ADDR 0b1101011
 // *****************************************************************************
 /* Application Data
 
@@ -97,6 +119,70 @@ APP_DATA appData;
 /* TODO:  Add any necessary local functions.
 */
 
+void initExp(){
+    ANSELBbits.ANSB2 = 0;
+    ANSELBbits.ANSB3 = 0;
+    i2c_master_setup();
+
+    writei2c(0x10,0b10000010);
+    writei2c(0x11,0b10001000);
+    writei2c(0x12,0b00000100);
+    
+
+}
+
+void writei2c(unsigned char reg, unsigned char val){
+    i2c_master_start();
+    i2c_master_send(ADDR <<1 | 0);
+    i2c_master_send(reg);
+    i2c_master_send(val);
+    i2c_master_stop();
+}
+
+
+unsigned char readi2c(unsigned char reg1){
+    i2c_master_start(); // make the start bit
+    i2c_master_send((ADDR << 1) | 0); // write the address, shifted left by 1, or'ed with a 0 to indicate writing
+    i2c_master_send(reg1); // the register to read from
+    i2c_master_restart(); // make the restart bit
+    i2c_master_send((ADDR << 1) | 1); // write the address, shifted left by 1, or'ed with a 1 to indicate reading
+    unsigned char r = i2c_master_recv(); // save the value returned
+    i2c_master_ack(1); // make the ack so the slave knows we got it 
+    i2c_master_stop(); // make the stop bit
+
+    return r;
+
+}
+
+
+void I2C_read_multiple(unsigned char reg2, unsigned short * data, int length){
+    
+    int i;
+
+    
+    i2c_master_start(); // make the start bit
+    i2c_master_send((ADDR << 1) | 0); // write the address, shifted left by 1, or'ed with a 0 to indicate writing
+    i2c_master_send(reg2); // the register to read from
+    i2c_master_restart(); // make the restart bit
+    i2c_master_send((ADDR << 1) | 1); // write the address, shifted left by 1, or'ed with a 1 to indicate reading
+    
+    for (i=0;i<length;i++){    
+        data[i]= i2c_master_recv();
+        
+        if (i<length-1){
+            i2c_master_ack(0);
+        }
+
+        else{
+            i2c_master_ack(1);
+        }
+        
+    }
+    i2c_master_stop();
+    
+    
+}
+
 
 // *****************************************************************************
 // *****************************************************************************
@@ -128,6 +214,15 @@ void APP_Initialize ( void )
     
     //Set B4 as input for LED
     TRISBbits.TRISB4 = 1;
+    
+
+
+    initExp();
+    LCD_init();
+    LCD_clearScreen(BLACK);
+    
+   
+    
 }
 
 
@@ -149,7 +244,7 @@ void APP_Tasks ( void )
         case APP_STATE_INIT:
         {
             bool appInitialized = true;
-       
+            
         
             if (appInitialized)
             {
@@ -162,13 +257,52 @@ void APP_Tasks ( void )
         case APP_STATE_SERVICE_TASKS:
         {
             
-            while(PORTBbits.RB4 == 1){
-                _CP0_SET_COUNT(0);
-                while(_CP0_GET_COUNT()<12000){
-                }
-    
-                LATAbits.LATA4 = !LATAbits.LATA4;
+            I2C_read_multiple(0x20, data, 14);
+            
+            accelX = data[8] | (data[9] << 8);
+            accelZ = data[12] | (data[13] << 8);
+            
+            
+            sprintf(message1, "X Accel %d      ", accelX);
+            LCD_drawString(10,5,message1, WHITE, BLACK);
+            
+            sprintf(message2, "Z Accel %d      ", accelZ);
+            LCD_drawString(10,20,message2, WHITE, BLACK);
+
+            
+            percentX = 50*accelX/range;
+            percentZ = 50*accelZ/range;
+            
+            
+            if (accelX>0){
+                LCD_drawProgressBox(64,80,2,percentX,50-percentX,WHITE,BLUE);
+                LCD_drawProgressBox(14,80,2,0,50,WHITE,BLUE);
+                
             }
+            
+            if (accelX<0){
+                LCD_drawProgressBox(14,80,2,50+percentX, -percentX,BLUE,WHITE);
+                LCD_drawProgressBox(64,80,2,0,50,WHITE,BLUE);
+                  
+            }
+            
+            if (accelZ>0){
+                LCD_drawProgressBoxVertical(63,30,2,50-percentZ,percentZ,BLUE,WHITE);
+                LCD_drawProgressBoxVertical(63,80,2,0,50,WHITE,BLUE);
+            }
+            
+            if (accelZ<0){
+                LCD_drawProgressBoxVertical(63,30,2,50,0,BLUE,WHITE);
+                LCD_drawProgressBoxVertical(63,80,2,-percentZ,50+percentZ,WHITE,BLUE);
+            }
+           
+      
+        
+            _CP0_SET_COUNT(0);
+            while(_CP0_GET_COUNT()<2400000){
+            }
+            LATAbits.LATA4 = !LATAbits.LATA4;
+            
             break;
         }
 
